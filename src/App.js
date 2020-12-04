4f31719744c5368dc8d852cad4d300cbd8430ac0
Jennifer Klein <inf18137@lehre.dhbw-stuttgart.de>
2020-12-04 12:26:34 +0100

    fixed some warnings

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ export default class App extends React.Component {
     let tmpItems = [...this.state.items];
     let tmpNodes = [...this.state.nodes];
     let insertedNodeIndex;
-    items.map((newItem) => {
+    items.foreach((newItem) => {
       //check if item is between 0 and 64000
       if (newItem < 0 || newItem > 64000 || isNaN(newItem)) {
         alert("Es dürfen nur Werte zwischen 0 und 64000 eingefügt werden...");
@@ -159,7 +159,7 @@ export default class App extends React.Component {
   deleteItem = (items) => {
     let tmpNodes = [...this.state.nodes];
     let itemId;
-    items.map((newItem) => {
+    items.foreach((newItem) => {
       let values = this.findItem(newItem);
       //Return, wenn ein Wert nicht im Baum existiert
       if (values.indexOfItem === undefined) {
@@ -330,7 +330,6 @@ export default class App extends React.Component {
               index = undefined;
               return true;
             }
-            let tmp = index;
             index = this.findIndexOfNode(tmpNodes[index].childs[itemIndex], tmpNodes);
             costs++;
             breakLoop = true;
@@ -340,7 +339,6 @@ export default class App extends React.Component {
               index = undefined;
               return true;
             }
-            let tmp = index;
             index = this.findIndexOfNode(tmpNodes[index].childs[itemIndex + 1], tmpNodes);
             costs++;
             breakLoop = true;
