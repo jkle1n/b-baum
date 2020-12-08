6794af02873e5c75fd6d50d2b24d63d7c7e9c9b7
Jennifer Klein <inf18137@lehre.dhbw-stuttgart.de>
2020-12-08 16:59:13 +0100

    fixed display tree

--- src/Baum.js
+++ src/Baum.js
@@ -7,23 +7,24 @@ export class Baum extends React.Component {
         let items = [];
         if (this.props.nodes.length !== 0) {
             let rootIndex = this.props.findIndexOfRoot();
-            rootIndex = rootIndex === -1 ? 0 : rootIndex;
-            let index = rootIndex;
-            items.push(<div style={{ display: "flex", flexDirection: "row", justifyContent: "center" }}><Node param={this.props.param} items={this.props.nodes[rootIndex].items} foundIndex={rootIndex === this.props.foundItem.nodeIndex ? this.props.foundItem.itemIndex : undefined} /><br /></div>);
-            let childs = this.props.nodes[index].childs;
-            let childIndex;
-            let newChilds = [];
-            let tmpItems = [];
-            while (childs.length !== 0) {
-                for (let j = 0; j < childs.length; j++) {
-                    childIndex = this.props.findIndexOfNode(childs[j]);
-                    tmpItems.push(<Node param={this.props.param} items={this.props.nodes[childIndex].items} foundIndex={childIndex === this.props.foundItem.nodeIndex ? this.props.foundItem.itemIndex : undefined} />);
-                    newChilds = newChilds.concat(this.props.nodes[childIndex].childs);
+            if (rootIndex !== -1) {
+                let index = rootIndex;
+                items.push(<div style={{ display: "flex", flexDirection: "row", justifyContent: "center" }}><Node param={this.props.param} items={this.props.nodes[rootIndex].items} foundIndex={rootIndex === this.props.foundItem.nodeIndex ? this.props.foundItem.itemIndex : undefined} /><br /></div>);
+                let childs = this.props.nodes[index].childs;
+                let childIndex;
+                let newChilds = [];
+                let tmpItems = [];
+                while (childs.length !== 0) {
+                    for (let j = 0; j < childs.length; j++) {
+                        childIndex = this.props.findIndexOfNode(childs[j]);
+                        tmpItems.push(<Node param={this.props.param} items={this.props.nodes[childIndex].items} foundIndex={childIndex === this.props.foundItem.nodeIndex ? this.props.foundItem.itemIndex : undefined} />);
+                        newChilds = newChilds.concat(this.props.nodes[childIndex].childs);
+                    }
+                    items.push(<div style={{ display: "flex", flexDirection: "row", justifyContent: "center" }}>{tmpItems}</div>);
+                    tmpItems = [];
+                    childs = newChilds;
+                    newChilds = [];
                 }
-                items.push(<div style={{ display: "flex", flexDirection: "row", justifyContent: "center" }}>{tmpItems}</div>);
-                tmpItems = [];
-                childs = newChilds;
-                newChilds = [];
             }
         }
         return items;
